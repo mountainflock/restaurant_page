e4f4f7304642595f0d3e0cd3176ba80c10fcc053
Marina <reddymikks@gmail.noreply.github.com>
2023-04-14 19:42:46 +0300

    Add basic functions structure

---
+++ src/home.js
@@ -0,0 +1,11 @@
+function loadHomePage() {
+  const content = document.querySelector("#content");
+  const mainText = document.createElement("h1");
+  mainText.textContent = "THIS IS A RESTAURANT PAGE";
+  const text = document.createElement("h3");
+  text.textContent = "The Restaurant is very good an the page is good as well.";
+  content.appendChild(mainText);
+  content.appendChild(text);
+}
+
+export default loadHomePage;
